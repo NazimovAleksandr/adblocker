46c10b9117bc29b656b3b5c508964edbb9f1c133
Назимов Александр <50_Sanya@mail.ru>
2024-07-08 16:55:05 +0500

    RustAdBlocker

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # adblocker
 Аn example of using an ad blocker
+
+```kotlin
+// getting the RustAdBlocker object
+val rustAdBlocker: RustAdBlocker = RustAdBlocker.instance()
+
+// using RustAdBlocker
+webView.webViewClient = object : WebViewClient() {
+    override fun shouldInterceptRequest(view: WebView?, request: WebResourceRequest?): WebResourceResponse? {
+        val url = request?.url.toString()
+
+        val isAdUrl: Boolean = rustAdBlocker.check(url)
+
+        if (isAdUrl) return null
+
+        return super.shouldInterceptRequest(view, request)
+    }
+}
+```
---
+++ app/src/main/java/com/test/adblocker/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.test.adblocker
+
+import android.os.Bundle
+import android.webkit.URLUtil
+import android.webkit.WebResourceRequest
+import android.webkit.WebResourceResponse
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val webView: WebView = findViewById(R.id.web_view)
+        val editText: EditText = findViewById(R.id.edit_text)
+        val button: Button = findViewById(R.id.button)
+
+        button.setOnClickListener {
+            var url = editText.text.toString()
+
+            if (!URLUtil.isValidUrl(url)) {
+                url = "https://www.google.com/search?q=$url"
+            }
+
+            webView.loadUrl(url)
+        }
+
+        val rustAdBlocker: RustAdBlocker = RustAdBlocker.get()
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldInterceptRequest(view: WebView?, request: WebResourceRequest?): WebResourceResponse? {
+                val url = request?.url.toString()
+
+                val isAdUrl: Boolean = rustAdBlocker.check(url)
+
+                if (isAdUrl) return null
+
+                return super.shouldInterceptRequest(view, request)
+            }
+        }
+    }
+}
